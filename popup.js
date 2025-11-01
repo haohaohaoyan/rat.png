f1892cee50309d3fa19f52d44e7ec89af0abffa9
Haoyan Li <haoyanli234@gmail.com>
2025-11-01 12:42:38 -0400

    added custom images

--- popup.js
+++ popup.js
@@ -1,9 +1,30 @@
 replaceToggle = document.querySelector('#replacetoggle')
+imageToggle = document.querySelector('#imagetoggle')
+customUrl = document.querySelector('#customurl')
+display = document.querySelector('#placeholder')
 
+
+//please tell me if there's a more efficient way to do this
 chrome.storage.local.get(['ok']).then((result) => {
     replaceToggle.checked = result['ok'];
 })
+chrome.storage.local.get(['customimage']).then((result) => {
+    customUrl.value = result['customimage'];
+    if (result['customimage']) {
+        display.src = result['customimage'];
+        display.setProperty('scale', '1');
+    }
+})
+chrome.storage.local.get(['usecustomimage']).then((result) => {
+    imageToggle.checked = result['usecustomimage'];
+});
 
 replaceToggle.addEventListener('change', function() {
     chrome.storage.local.set({'ok': this.checked})
+});
+customUrl.addEventListener('change', function() {
+    chrome.storage.local.set({'customimage': this.value})
+});
+imageToggle.addEventListener('change', function() {
+    chrome.storage.local.set({'usecustomimage': this.checked})
 });
--- replacer.js
+++ replacer.js
@@ -1,16 +1,19 @@
 function replaceImages() {
     chrome.storage.local.get(['ok']).then((result) => {
-        let enabled = result['ok'];
-        if (enabled == true) {
-            for (const image of document.querySelectorAll('img')) {
-                image.src = 'https://lh3.googleusercontent.com/d/18XofteYxRGj4_JBrwMETVE3cNhSjo32t'
+        if (result['ok'] == true) {
+            for (const image of document.querySelectorAll('img, iframe')) {
+                chrome.storage.local.get(['usecustomimage']).then((result) => {
+                    if (result['usecustomimage'] == true) {
+                        chrome.storage.local.get(['customimage']).then((result) => {
+                            image.src = result['customimage'];
+                        });
+                    } else {
+                        image.src = 'https://lh3.googleusercontent.com/d/18XofteYxRGj4_JBrwMETVE3cNhSjo32t'
+                    }
+                })
                 image.style.setProperty('image-rendering', 'pixelated')
                 image.style.setProperty('object-fit', 'fill')
-            }
-            for (const image of document.querySelectorAll('iframe')) {
-                image.src = 'https://lh3.googleusercontent.com/d/18XofteYxRGj4_JBrwMETVE3cNhSjo32t'
-                image.style.setProperty('image-rendering', 'pixelated')
-                image.style.setProperty('object-fit', 'fill')
+                image.style.setProperty('margin', '0')
             }
         }
     });
